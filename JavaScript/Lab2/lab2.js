35b6ff58e48ee3a45ce51682406bb1478bca05cc
vladimirbagrov <126449691+vladimirbagrov@users.noreply.github.com>
2023-05-16 10:08:08 +0300

    1

--- JavaScript/Lab2/lab2.js
+++ JavaScript/Lab2/lab2.js
@@ -5,9 +5,14 @@ function pow(x, n) {
 }
 
 function sumTo(n) {
+    //вычисляет сумму чисел от 1 до n включительно
     let sum = 0;
     for (let i = 1; i <= n; i++) {
         sum += i
     }
     return sum;
+}
+
+function factorial(n) {
+    return (n != 1) ? n = factorial(n - 1) : 1;
 }
