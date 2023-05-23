dbadaca4713970279f0c8cf9eeea9ae76c71a533
vladimirbagrov <126449691+vladimirbagrov@users.noreply.github.com>
2023-05-23 12:28:54 +0300

    -

--- JavaScript/Lab2/lab2.js
+++ JavaScript/Lab2/lab2.js
@@ -1,7 +1,7 @@
 function pow(x, n) {
     //Функция возвращает x в степени n
     let result = x ** n;
-    return result;
+    return result;//возвращаем х в степени n
 }
 
 function sumTo(n) {
@@ -10,10 +10,52 @@ function sumTo(n) {
     for (let i = 1; i <= n; i++) {
         sum += i
     }
-    return sum;
+    return sum;//возвращаем сумму
 }
 
 function factorial(n) {
     //возвращает факториал числа n 
     return (n != 1) ? n = factorial(n - 1) : 1;
+}
+
+function fib(n){
+    //функция возвращает n-е число Фибоначчи
+    a = BigInt(1); // первое число Фиббоначи
+    b=BigInt(1); //второе число Фиббоначи
+    if (n== BigInt(0))
+    {
+        return BigInt(0); //возвращаем ноль при n=0
+    }
+    else
+    { 
+        for (i = BigInt(3); i<=n; i++) //алгоритм фибоначчи
+        {
+            c = a+b;
+            a=b;
+            b=c;
+        }
+        return b;
+    }
+}
+
+//функция принимает целочисленное значение x и возвращает анонимную функцию. Анонимная функция возвращает результат сравнения значений y и x
+function compare(x)
+{
+return function(y)
+{
+    if (y>x) return true;
+    else if (y<x) return false;
+    else if (y==x) return null;
+}
+}
+
+//функция возвращает сумму всех своих аргументов.
+function sum()
+{
+    var result=0; 
+    for (var i=0; i<arguments.length; i++)
+    {
+        result+=arguments[i] 
+    }
+    return result;
 }
