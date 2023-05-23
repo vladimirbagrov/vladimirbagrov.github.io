b2c8a6cb389ec2e8c31dfa0878c720c355d59cb8
vladimirbagrov <126449691+vladimirbagrov@users.noreply.github.com>
2023-05-23 12:43:37 +0300

    7

--- JavaScript/Lab3/lab3.js
+++ JavaScript/Lab3/lab3.js
@@ -1,7 +1,15 @@
-function getDecimal(num) {
-    var str = "" + num;
-    var zeroPos = str.indexOf(".");
-    if (zeroPos == -1) return 0;
-    str = str.slice(zeroPos);
-    return +str;
-  }
+function getDecimal(num)
+{
+    var str = "" + num; // 1
+ var zeroPos = str.indexOf("."); // 2
+
+ if (zeroPos == -1) return 0; // 3
+ str = str.slice(zeroPos); // 4
+
+ return +str; // 5
+}
+ucFirst(str)
+{
+    if (!str) return str;
+    return str[0].toUpperCase() + str.slice(1);
+}
