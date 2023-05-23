9d4f2dd9299df981ecb5eaa22f72ac8b701db7d6
vladimirbagrov <126449691+vladimirbagrov@users.noreply.github.com>
2023-05-23 12:48:48 +0300

    5

--- JavaScript/Lab3/lab3.js
+++ JavaScript/Lab3/lab3.js
@@ -1,15 +1,51 @@
-function getDecimal(num)
-{
-    var str = "" + num; // 1
- var zeroPos = str.indexOf("."); // 2
-
- if (zeroPos == -1) return 0; // 3
- str = str.slice(zeroPos); // 4
-
- return +str; // 5
+export function getDecimal(num) {
+    if (Math.trunc(num) == num) {
+        return 0;
+    }
+    else if (num < 0) {
+        return num - (-Math.ceil(-num));
+    }
+    if (num > 1) {
+        return (num - 1).toFixed(2);
+    }
 }
-ucFirst(str)
-{
-    if (!str) return str;
-    return str[0].toUpperCase() + str.slice(1);
+
+
+
+export function ucFirst(str) {
+    if (str == null) {
+        return '';
+    }
+    else {
+        let NewStr = str[0].toUpperCase() + str.slice(1);
+        return NewStr;
+    }
+}
+
+
+
+export function checkSpam(str) {
+    let NewStr = str.toLowerCase();
+    return NewStr.includes('viagra') || NewStr.includes('xxx');
+}
+
+
+
+export function truncate(str, maxlength) {
+    if (str.length < maxlength) {
+        return str;
+    }
+    else {
+        return str.slice(0, maxlength - 1) + '…';
+    }
+}
+
+
+
+export function camelize(str) {
+    let StrNew = str.split('-');
+    for (let i = 0; i <= StrNew.lenght; i++) {
+        console.log(StrNew[i]);
+    }
+    return StrNew.join('');
 }
