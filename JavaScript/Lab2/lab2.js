f90aafd3aafa94b9a28b7ea3004ae6e467f30a63
vladimirbagrov <126449691+vladimirbagrov@users.noreply.github.com>
2023-06-05 19:04:22 +0300

    1

--- JavaScript/Lab2/lab2.js
+++ JavaScript/Lab2/lab2.js
@@ -1,66 +1,63 @@
+'use strict';
+//функция возвращает x в степени n (n - целое число).
 function pow(x, n) {
-    //Функция возвращает x в степени n
-    let result = x ** n;
-    return result;//возвращаем х в степени n
+    return x ** n; //возвращаем х в степени n
 }
-
+// функция вычисляет сумму чисел от 1 до n включительно.
 function sumTo(n) {
-    //вычисляет сумму чисел от 1 до n включительно
-    let sum = 0;
-    for (let i = 1; i <= n; i++) {
-        sum += i
-    }
-    return sum;//возвращаем сумму
+    return (n * (n + 1)) / 2  //формула суммы арифметической прогрессии
+
 }
 
-    
-function factorial(n) {  
-    //возвращает факториал числа n 
-    if (n<=1) return 1n;
+/**
+ * Возвращает факториал от n
+ * @param {number} n факториал этого числа
+ * @returns Факториал от n
+ */
+function factorial(n) {
+    if (n <= 0) {
+        return 1n; //возвращаем 1 для чисел, меньших 0
+    }
     else {
-        res = BigInt (n) * BigInt(factorial(n-1))
-        return res;
-    }  
-}
-
-function fib(n){
-    //функция возвращает n-е число Фибоначчи
-    a = BigInt(1); // первое число Фиббоначи
-    b=BigInt(1); //второе число Фиббоначи
-    if (n== BigInt(0))
-    {
-        return BigInt(0); //возвращаем ноль при n=0
+        return (BigInt(n) * BigInt(factorial(n - 1))); //использование рекурсии 
     }
-    else
-    { 
-        for (i = BigInt(3); i<=n; i++) //алгоритм фибоначчи
-        {
-            c = a+b;
-            a=b;
-            b=c;
+}
+/**
+ * функция возвращает n-е число Фибоначи
+ * @param {number} n 
+ * @returns n-е число Фибоначи
+ */
+function fib(n) {
+    let a = BigInt(1); // первое число Фиббоначи
+    let b = BigInt(1); //второе число Фиббоначи
+    if (n == BigInt(0)) {
+        return BigInt(0); //возвращаем нуль при n=0
+    }
+    else {
+        //алгоритм нахождения чисел Фиббоначи (базовый)
+        for (let i = BigInt(3); i <= n; i++) {
+            let c = a + b;
+            a = b;
+            b = c;
         }
         return b;
     }
 }
-
-//функция принимает целочисленное значение x и возвращает анонимную функцию. Анонимная функция возвращает результат сравнения значений y и x
-function compare(x)
-{
-return function(y)
-{
-    if (y>x) return true;
-    else if (y<x) return false;
-    else if (y==x) return null;
+//функция принимает целочисленное значение x и возвращает 
+//анонимную функцию. Анонимная функция возвращает 
+//результат сравнения значений y и x
+function compare(x) {
+    return function (y) {
+        if (y > x) return true;
+        else if (y < x) return false;
+        else if (y == x) return null;
+    }
 }
-}
-
 //функция возвращает сумму всех своих аргументов.
-function sum()
-{
-    var result=0; 
-    for (var i=0; i<arguments.length; i++)
-    {
-        result+=arguments[i] 
+function sum() {
+    var result = 0; //сумма
+    for (var i = 0; i < arguments.length; i++) {
+        result += arguments[i] //складываем элементы на соответствующих местах
     }
     return result;
 }
--- JavaScript/Lab3/lab3.js
+++ JavaScript/Lab3/lab3.js
@@ -1,37 +1,32 @@
-export function getDecimal(num) {
+'use strict';
+export function getDecimal(num) { //возвращает дробную часть числа num.
     if (Math.trunc(num) == num) {
         return 0;
     }
     else if (num < 0) {
         return num - (-Math.ceil(-num));
     }
+
     if (num > 1) {
-        return (num - 1).toFixed(2);
+        return +(num - Math.trunc(num)).toFixed(2);
     }
 }
 
-
-
-export function ucFirst(str) {
-    if (str == null) {
-        return '';
+export function ucFirst(str) { //возвращает строку str с заглавным первым символом.
+    if (!str) {
+        return str;
     }
     else {
-        let NewStr = str[0].toUpperCase() + str.slice(1);
-        return NewStr;
+        return str[0].toUpperCase() + str.slice(1);
     }
 }
 
-
-
-export function checkSpam(str) {
-    let NewStr = str.toLowerCase();
-    return NewStr.includes('viagra') || NewStr.includes('xxx');
+export function checkSpam(str) { //возвращает true, если строка str содержит 'viagra' или 'XXX', а иначе false. Функция должна быть нечувствительна к регистру.
+    let newstr = str.toLowerCase();
+    return newstr.includes('viagra') || NewStr.includes('xxx');
 }
 
-
-
-export function truncate(str, maxlength) {
+export function truncate(str, maxlength) { //проверяет длину строки str, и если она превосходит maxlength – заменяет конец str на символ многоточие "…"
     if (str.length < maxlength) {
         return str;
     }
@@ -40,12 +35,29 @@ export function truncate(str, maxlength) {
     }
 }
 
-
-
-export function camelize(str) {
-    let StrNew = str.split('-');
-    for (let i = 0; i <= StrNew.lenght; i++) {
-        console.log(StrNew[i]);
+export function camelize(str) { //преобразуйте строку вида 'var-test-text' в 'varTestText'. То есть, дефисы удаляются, а все слова после них получают заглавную букву. 
+    let stroka = str.split("-");
+    let strnw = "";
+    for (let i = 0; i <= stroka.length - 1; i++) {
+        if (i == 0) strnw = stroka[0];
+        else strnw += ucFirst(stroka[i]);
     }
-    return StrNew.join('');
+    return strnw;
+}
+
+import { fib } from './lab2.js';
+export function fibs(n) {  //для натурального n возвращает массив, заполненный числами Фибоначчи до n-го (не включая его).
+    let mas = [];
+    for (let i = 0; i < n; i += 1) {
+        mas.push(fib(i));
+    }
+    return mas;
+}
+export function arrReverseSorted(arr) { //принимает неупорядоченный массив чисел arr и возвращает массив из тех же элементов, но отсортированный по убыванию
+    return arr.sort(function (a, b) {
+        return b - a;
+    })
+}
+export function unique(arr) { //вернёт массив уникальных, не повторяющихся значений массива arr. Значения в массиве могут быть любого типа.
+    return Array.from(new Set(arr));
 }
